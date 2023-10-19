8cc8ed62abc1213be403ca3deb9ee22c28b38e9b
jasurbek <jasurdev1604@gmail.com>
2023-10-19 19:52:20 +0500

    Node JS file System

---
+++ data.js
@@ -0,0 +1,3 @@
+const text = "jasurbek";
+
+module.exports = text;
--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log("hello node js");
+const text = require("./data");
+
+console.log(text);
+
+console.log(__dirname); // => /home/jasurbek/Desktop/NodeJs`
+console.log(__filename); // =>/home/jasurbek/Desktop/NodeJs/index.js
