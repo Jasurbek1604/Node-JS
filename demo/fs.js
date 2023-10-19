dc6f3ecfb7a16138a2d17f4fb5068c2412f11ee0
jasurbek <jasurdev1604@gmail.com>
2023-10-19 20:09:05 +0500

    Node JS file System

--- demo/fs.js
+++ demo/fs.js
@@ -15,3 +15,24 @@ const removeDir = () => {
     console.log("directory successfully created!");
   });
 };
+
+const filePath = path.join(__dirname, "test", "test.txt");
+
+// createDir();
+
+// fs.writeFile(filePath, "Hello NodeJS", (err) => {
+//   if (err) throw err;
+//   console.log("file created");
+// });
+
+// fs.appendFile(filePath, "\nHello baby", (err) => {
+//   if (err) throw err;
+//   console.log("file appended");
+// });
+
+fs.readFile(filePath, "utf-8", (err, data) => {
+  // if (err) throw err;
+  // const content = new Buffer.from(data);
+  // console.log(content.toString());
+  console.log(data);
+});
